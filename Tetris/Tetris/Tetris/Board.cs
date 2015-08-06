52d8be3c23a0067c7126a75f17b6a2bbe237a4be
Zopherus <ejz1999@yahoo.com>
2015-08-05 23:10:02 -0700

    Made some classes

---
+++ Tetris/Tetris/Tetris/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    class Board
+    {
+        //The board is only 10 by 20, but the border of 1 around act as the walls
+        //bool is true if there is block in that space
+        bool[,] boardState = new bool[TetrisGame.gridWidth + 2, TetrisGame.gridHeight + 2];
+
+
+    }
+}
---
+++ Tetris/Tetris/Tetris/Piece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    class Piece
+    {
+        private BlockType pieceType;
+        private Block[] blocks;
+    }
+}
